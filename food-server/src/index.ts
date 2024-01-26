be9d2b0f4ea755a07a9985519f2468beb536d454
Bilguun Banzragch <LP230113@G301-CC0208.local>
2024-01-26 19:19:02 +0800

    sendmail

--- food-server/src/index.ts
+++ food-server/src/index.ts
@@ -1,6 +1,31 @@
 import express, { Application } from "express";
 import color from "colors";
+import { connectDB } from "./config/db";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes";
+import verifyRoutes from "./routes/verifyRoutes";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI as string;
 
 const app: Application = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(8080, () => console.log(color.bgGreen.black("Server is running")));
+connectDB(MONGO_URI);
+
+// app.get("/user", async (req: Request, res: Response) => {
+//   const newUser = {
+//     name: "Admin",
+//     email: "admin@gmail.com",
+//     password: "admin123",
+//   };
+//   res.json({ message: "Shine hereglegch uuslee" });
+// });
+
+app.use("/auth", authRoutes);
+app.use("/verify", verifyRoutes);
+
+app.listen(8080, () => console.log(color.rainbow("Server is running")));

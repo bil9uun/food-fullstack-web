8e434ec8c42b267ed4c5c5307a7ed88d46ceeb9d
Bilguun Banzragch <LP230113@G301-CC0208.local>
2024-02-07 18:54:02 +0800

    admin front categoryModal, foodmodal nemev

---
+++ food-admin/src/components/core/input/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import React, { ChangeEvent, useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+interface IInputProps {
+  name?: string;
+  desc?: string;
+  label: string;
+  value?: string;
+  errorText?: string | undefined;
+  showPassword?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input = ({
+  label,
+  value,
+  showPassword = false,
+  onChange,
+  errorText,
+  desc,
+  name,
+}: IInputProps) => {
+  const [isShowPassword, setIsShowPassword] = useState(showPassword);
+  return (
+    <FormControl sx={{ my: "1rem" }} variant="outlined" fullWidth>
+      <Stack spacing={1}>
+        <FormLabel sx={{ my: "4px", color: "black" }}>{label}</FormLabel>
+        <OutlinedInput
+          value={value}
+          name={name}
+          onChange={onChange}
+          sx={{ backgroundColor: "#ECEDF0" }}
+          placeholder={label}
+          type={isShowPassword ? "password" : "text"}
+          endAdornment={
+            showPassword && (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => {
+                    setIsShowPassword(!isShowPassword);
+                  }}
+                >
+                  {isShowPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            )
+          }
+        />
+      </Stack>
+      <FormHelperText error={errorText ? true : false}>
+        {errorText}
+      </FormHelperText>
+    </FormControl>
+  );
+};
